4ee74eba18562cb5fa6feb19ba8e0e741da86b43
ArcusMarkus <belmonteMM@hendrix.edu>
2026-05-09 19:20:34 -0500

    Initial project created

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace csci340lab8.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
